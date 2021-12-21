aff9e4fee107af58fd9890f9f6a57add2b1a83b1
titanXL <emildavidkov88@gmail.com>
2021-12-21 18:45:08 +0200

    test cd

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Hello NextJS</h1>
       </main>
 
       <footer className={styles.footer}></footer>
